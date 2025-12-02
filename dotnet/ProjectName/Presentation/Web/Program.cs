db0b46875c3903c2eba9fc9d1d1e9dbec4580319
Ismail-soo <132010906+Ismail-soo@users.noreply.github.com>
2025-12-02 10:16:00 +0700

    a

--- dotnet/ProjectName/Presentation/Web/Program.cs
+++ dotnet/ProjectName/Presentation/Web/Program.cs
@@ -5,8 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
-using Serilog.Sinks.ApplicationInsights.Sinks.ApplicationInsights.TelemetryConverters;
 using Serilog.Sinks.SystemConsole.Themes;
+using Serilog.Sinks.ApplicationInsights.Sinks.ApplicationInsights.TelemetryConverters;
 
 namespace Web
 {
@@ -14,20 +14,19 @@ namespace Web
     {
         public static int Main(string[] args)
         {
-            // Setup dasar Serilog
             var loggerConfig = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.Console(theme: AnsiConsoleTheme.Code);
 
-            // Application Insights (optional)
             var aiKey = Environment.GetEnvironmentVariable("WEB_APPLICATION_INSIGHTS_KEY");
+
             if (!string.IsNullOrWhiteSpace(aiKey))
             {
                 loggerConfig = loggerConfig.WriteTo.ApplicationInsights(
                     aiKey,
-                    new TraceTelemetryConverter()   // ← ini COMPATIBLE .NET 8
+                    new TraceTelemetryConverter()
                 );
             }
 
@@ -39,20 +38,16 @@ namespace Web
 
                 var builder = WebApplication.CreateBuilder(args);
 
-                // Integrasi Serilog resmi .NET 8
                 builder.Host.UseSerilog();
 
-                // Kompatibilitas dengan Startup.cs lama
-                var startupLogger = Log.ForContext<Startup>();
-                var startup = new Startup(builder.Configuration, startupLogger);
-
+                var startup = new Startup(builder.Configuration, 
+                    Log.Logger.ForContext<Startup>());
                 startup.ConfigureServices(builder.Services);
 
                 var app = builder.Build();
                 startup.Configure(app, app.Environment);
 
                 app.Run();
-
                 return 0;
             }
             catch (Exception ex)
